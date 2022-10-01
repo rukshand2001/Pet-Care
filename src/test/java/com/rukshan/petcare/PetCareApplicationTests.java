b3d5d3117305961a66c78cc0d4841e0d3a6f6b17
rukshan_d <rukshand20012@gmail.com>
2022-10-01 15:23:51 +0530

    Hack project

---
+++ src/test/java/com/rukshan/petcare/PetCareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rukshan.petcare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetCareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
