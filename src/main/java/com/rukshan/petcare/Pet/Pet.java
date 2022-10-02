5f9debc5e44247616b65bb42af734cabb7d78113
rukshan_d <rukshand20012@gmail.com>
2022-10-02 11:42:15 +0530

    project pet class resolved

--- src/main/java/com/rukshan/petcare/Pet/Pet.java
+++
@@ -1,6 +0,0 @@
-package com.rukshan.petcare.Pet;
-
-public class Pet {
-    }
-
-
--- src/main/java/com/rukshan/petcare/Pet/PetServices.java
+++ src/main/java/com/rukshan/petcare/Pet/PetServices.java
@@ -3,15 +3,12 @@ package com.rukshan.petcare.Pet;
 import com.rukshan.petcare.utils.Message;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 @Service
 public class PetServices {
 
     public  Message ping(){
+        /* calling ping function */
 
-        return ping();
+        return new Message("Hello pet",200);
     }
 }
